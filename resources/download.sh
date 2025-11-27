7072d716462952a7066f5104ee627f8590bc9728
Danny <mian532488@gmail.com>
2025-11-27 09:35:12 +0800

    Optimize structure, use new tokenizer training data and The Pile dataset.

--- resources/download.sh
+++ resources/download.sh
@@ -1,6 +1,9 @@
-wget https://www.gutenberg.org/cache/epub/77331/pg77331.txt
-wget https://www.gutenberg.org/cache/epub/77330/pg77330.txt
-wget https://www.gutenberg.org/cache/epub/77328/pg77328.txt
-wget https://www.gutenberg.org/cache/epub/1342/pg1342.txt
-wget https://www.gutenberg.org/cache/epub/8492/pg8492.txt
-wget https://www.gutenberg.org/cache/epub/43/pg43.txt
+# wget https://www.gutenberg.org/cache/epub/77331/pg77331.txt
+# wget https://www.gutenberg.org/cache/epub/77328/pg77328.txt
+# wget https://www.gutenberg.org/cache/epub/1342/pg1342.txt
+# wget https://www.gutenberg.org/cache/epub/8492/pg8492.txt
+# wget https://www.gutenberg.org/cache/epub/43/pg43.txt
+wget https://www.gutenberg.org/cache/epub/2542/pg2542.txt
+wget https://www.gutenberg.org/cache/epub/521/pg521.txt
+wget https://www.gutenberg.org/cache/epub/829/pg829.txt
+wget https://www.gutenberg.org/cache/epub/61522/pg61522.txt
--- train.py
+++ train.py
@@ -6,33 +6,15 @@ from model import BiagramLanguageModel
 
 # -----------------------------------------------------------
 # Hyperparameters
+MAX_TRAIN_SIZE = 10 * 1024 * 1024
 BATCH_SIZE = 32
 BLOCK_SIZE = 8
 EPOCHS = 30000
-EVAL_INTERVAL = 300
+eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 EVAL_ITERS = 200
 # -----------------------------------------------------------
-
-
-with open("./resources/harrypotter.txt", 'r') as f:
-    text = f.read()
-
-
-token = Tokenizer.load_json(r"resources/TokenizerModel.json")
-
-# Turn our text into tensor
-data = torch.tensor(token.encode(text), dtype=torch.long)
-# print(data.shape, data.dtype)
-
-# Split up the data into train and validation sets
-n = int(0.9*len(data))
-train_data = data[:n]
-val_data = data[n:]
-
-torch.manual_seed(1024)
-vocab_size = len(token.vocab)
 
 def get_batch(data:torch.Tensor, batch_size, block_size):
     # Generate random start indexs from 0 to len(data) - block_size 
@@ -43,16 +25,42 @@ def get_batch(data:torch.Tensor, batch_size, block_size):
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     return x, y
 
-blm = BiagramLanguageModel(vocab_size=vocab_size).to(device)
-optimizer = torch.optim.AdamW(blm.parameters(), lr = 1e-3)
+@torch.no_grad()
+def estimate_loss(model:torch.nn.Module):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(EVAL_ITERS)
+        for k in range(EVAL_ITERS):
+            xb, yb = get_batch(train_data if split == 'train' else val_data, 
+                               batch_size=BATCH_SIZE, block_size=BLOCK_SIZE)
+            xb, yb = xb.to(device), yb.to(device)
+            logits, loss = model(xb, yb)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
 def train(n_epochs, batch_size, block_size, 
           model:torch.nn.Module,
           optimizer:torch.optim.Optimizer):
-    for step in tqdm(range(n_epochs), desc="Training progress"):
+    # for step in tqdm(range(n_epochs), desc="Training progress"):
+    pbar = tqdm(range(n_epochs), desc="Training progress")
+    for step in pbar:
         # sample a batch of data
         xb, yb = get_batch(train_data, batch_size=4, block_size=8)
         xb, yb = xb.to(device), yb.to(device) 
+
+        if step % eval_interval == 0 or step == n_epochs - 1:
+            losses = estimate_loss(model)
+            # Update the info shown RIGHT NEXT to the progress bar
+            # pbar.set_postfix({
+            #     "Train Loss": f"{losses['train']:.4f}",
+            #     "Val Loss": f"{losses['val']:.4f}"
+            # })
+            
+            # We use tqdm.write instead of print() to avoid messing up the bar
+            tqdm.write(f"Step {step}: Train {losses['train']:.4f}, Val {losses['val']:.4f}")
 
         # evaluate the loss
         logits, loss = model(xb, yb)
@@ -62,13 +70,34 @@ def train(n_epochs, batch_size, block_size,
 
     print(loss.item())
 
-train(EPOCHS,
-      batch_size=BATCH_SIZE,
-      block_size=BLOCK_SIZE,
-      model=blm,
-      optimizer=optimizer
-      )
 
-sentences = "Harry"
-inputs = torch.tensor(token.encode(sentences)).unsqueeze(0).to(device)
-print(token.decode((blm.generate(inputs, max_new_tokens=100)[0].tolist())))
+if __name__ == "__main__":
+    with open("/media/danny/SuperMoose/Data/nanoGPT/pile_clean.txt", 'r') as f:
+        text = f.read(MAX_TRAIN_SIZE)
+
+    token = Tokenizer.load_json(r"resources/TokenizerModel.json")
+
+    # Turn our text into tensor
+    data = torch.tensor(token.encode(text), dtype=torch.long)
+    # print(data.shape, data.dtype)
+
+    # Split up the data into train and validation sets
+    n = int(0.9*len(data))
+    train_data = data[:n]
+    val_data = data[n:]
+
+    torch.manual_seed(1024)
+    vocab_size = len(token.vocab)
+
+    blm = BiagramLanguageModel(vocab_size=vocab_size).to(device)
+    optimizer = torch.optim.AdamW(blm.parameters(), lr = 1e-3)
+
+    train(EPOCHS,
+        batch_size=BATCH_SIZE,
+        block_size=BLOCK_SIZE,
+        model=blm,
+        optimizer=optimizer)
+
+    sentences = "Working over the theme, I found that as I proceeded, "
+    inputs = torch.tensor(token.encode(sentences)).unsqueeze(0).to(device)
+    print(token.decode((blm.generate(inputs, max_new_tokens=100)[0].tolist())))
